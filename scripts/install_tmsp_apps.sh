bcab750a2720d4d25fc667a5aa0ef73ec70b7bab
edatw <bladecad@gmail.com>
2016-11-13 15:12:20 +0800

    Update install_tmsp_apps.sh

--- scripts/install_tmsp_apps.sh
+++ scripts/install_tmsp_apps.sh
@@ -5,7 +5,7 @@ go get github.com/tendermint/tmsp/...
 # get the tmsp commit used by tendermint
 COMMIT=`bash scripts/glide/parse.sh $(pwd)/glide.lock tmsp`
 
-cd $GOPATH/src/github.com/tendermint/tmsp
+cd $GOPATH/src/github.com/edatw/tmsp
 git checkout $COMMIT
 go install ./cmd/...
 
